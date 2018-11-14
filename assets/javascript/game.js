5b20d50df0ea30e0ac553bd0455542d81d0c4237
Andrea Minhas <andreaminhas@Andreas-MacBook-Pro.local>
2018-11-13 19:26:12 -0600

    crystals are now cickable, random numbers are being generated and your score is being displayed.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,9 @@
-//set a global variable for the number to guess 
-var targetNumber = 28;
+//global variables
+var targetNumber = Math.floor(Math.random() * (120 - 19) + 19);
+var wins = 0;
+var losses = 0;
+var count = 0;
+var crystalWorth = Math.floor(Math.random() * (12 - 1) + 1);
 
 //Add the title into the header tag
 $("#title").html("<h1> The Crystal Collector</h2>");
@@ -7,67 +11,36 @@ $("#title").html("<h1> The Crystal Collector</h2>");
 //Add text to the screen inidcating what the target number is
 $("#number-to-guess").text("Number to Guess: " + targetNumber);
 
-var counter = 0;
-//creating an array of the values that each crystal will have
-var crystalWorth = [10, 8, 6, 2];
+//creating an array for the crystal images
+var imageList = [
+  "assets/images/1.png",
+  "assets/images/2.png",
+  "assets/images/3.png",
+  "assets/images/4.png"
+];
 
-$("#crystal-1").html('<img src="assets/images/1.png">');
-$("#crystal-2").html('<img src="assets/images/2.png">');
-$("#crystal-3").html('<img src="assets/images/3.png">');
-$("#crystal-4").html('<img src="assets/images/4.png">');
-
-for (var i = 0; i < crystalWorth.length; i++) {
-
-    var crystalImages = $("<img>");
-    crystalImages.addClass("crystal-images");
-    crystalImages.attr("data-crystalvalue", crystalWorth[i]);
-    $("#crystals").append(crystalImages);
+function createsImages() {
+  for (var i = 0; i < crystalWorth; i++) {
+    var crystalImage = $("<img>");
+    crystalImage.addClass("crystal-image");
+    crystalImage.attr("src", imageList[i]);
+    crystalImage.attr("data-crystalvalue", crystalWorth);
+    $("#crystals").append(crystalImage);
+  }
 }
-$(".crystal-images").on("click", function () {
-    var crystalNumbers = ($(this).attr("data-crystalvalue"));
-    crystalNumbers = parseInt(crystalNumbers);
-    counter += crystalNumbers;
-    alert("Your Score: " + counter);
-    if (counter === targetNumber) {
-        alert("You Win");
-        
-    }
-    else if (count > targetNumber) {
-        alert("You Lose!");  
-    }
+// $("#crystals").on("click", ".crystal-image", function() {
+$(document).on("click", ".crystal-image", function() {
+  var crystalNumbers = crystalWorth;
+  // crystalNumbers = parseInt(crystalNumbers);
+  console.log(crystalNumbers);
+  count += crystalNumbers;
+  $("#score").text("Your Score: " + count);
+
+  if (count === targetNumber) {
+    alert("You Win");
+  } else if (count > targetNumber) {
+    alert("You Lose!");
+  }
 });
 
-
-//     // Each imageCrystal will be given a data attribute called data-crystalValue.
-//     // This data attribute will be set equal to the array value.
-//     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-//     // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-//     $("#crystals").append(imageCrystal);
-//   }
-
-//   // This time, our click event applies to every single crystal on the page. Not just one.
-//   
-//     // Determining the crystal's value requires us to extract the value from the data attribute.
-//     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-//     // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-//     // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
-//     var crystalValue = ($(this).attr("data-crystalvalue"));
-//     crystalValue = parseInt(crystalValue);
-//     // We then add the crystalValue to the user's "counter" which is a global variable.
-//     // Every click, from every crystal adds to the global counter.
-//     counter += crystalValue;
-
-//     // All of the same game win-lose logic applies. So the rest remains unchanged.
-//     alert("New score: " + counter);
-
-//     if (counter === targetNumber) {
-//       alert("You win!");
-//     }
-
-//     else if (counter >= targetNumber) {
-//       alert("You lose!!");
-//     }
-
-//   });
+createsImages();
